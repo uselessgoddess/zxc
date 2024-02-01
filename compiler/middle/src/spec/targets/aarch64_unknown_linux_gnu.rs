2fd113244b5580828bef40fd21377aa5ab1dd4c7
uselessgoddess <dodickgod@gmail.com>
2024-02-01 15:32:19 +0300

    Fix data layout at `aarch64-unknown-linux-gnu` target

--- compiler/middle/src/spec/targets/aarch64_unknown_linux_gnu.rs
+++ compiler/middle/src/spec/targets/aarch64_unknown_linux_gnu.rs
@@ -1,10 +1,13 @@
-use crate::spec::{base, Target, TargetOptions};
+use {
+    crate::spec::{base, Target, TargetOptions},
+    macros::target_data_layout,
+};
 
 pub fn target() -> Target {
     Target {
         triple: "aarch64-unknown-linux-gnu".into(),
         pointer_width: 64,
-        data_layout: "e-m:e-i8:8:32-i16:16:32-i64:64-i128:128-n32:64-S128".into(),
+        data_layout: target_data_layout!("e-m:e-i8:8:32-i16:16:32-i64:64-i128:128-n32:64-S128"),
         arch: "aarch64".into(),
         options: TargetOptions { ..base::linux_gnu::opts() },
     }
